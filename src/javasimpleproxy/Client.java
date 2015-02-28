1c2e654f810f449707571ad7330f4147db34dbe6
guquabc <guquabc@gmail.com>
2015-03-01 00:22:51 +0800

    可运行

--- src/javasimpleproxy/Client.java
+++ src/javasimpleproxy/Client.java
@@ -5,6 +5,11 @@
  */
 package javasimpleproxy;
 
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author lzp
@@ -14,8 +19,42 @@ public class Client {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
+        //Socket serverSocket = new Socket("106.187.95.66", 30001);
+
+        //ServerSocket ss = new ServerSocket(30000);
+        ServerSocket ss = new ServerSocket(7202);
+
+        while (true) {
+            Socket browserSocket = ss.accept();
+
+            
+            
+            InputStream browserIn = browserSocket.getInputStream();
+            BufferedOutputStream outBrowser = new BufferedOutputStream(browserSocket.getOutputStream());
+
+            System.out.println("accept browser\n");
+            //Socket serverSocket = new Socket("127.0.0.1", 30001);
+            Socket serverSocket = new Socket("106.187.95.66", 30001);
+            InputStream serverIn = serverSocket.getInputStream();
+            BufferedOutputStream outServer = new BufferedOutputStream(serverSocket.getOutputStream());
+
+            
+            System.out.println("connect server\n");
+            new Thread(new ThreadedHandler(browserIn, outServer,browserSocket,serverSocket)).start();
+            new Thread(new ThreadedHandler(serverIn, outBrowser,browserSocket,serverSocket)).start();
+//
+//            byte[] arr = new byte[2048];
+//            int readbytes = 0;
+//            while ((readbytes = serverIn.read(arr)) > 0) {
+//                for (int i = 0; i < arr.length; i++) {
+//                    arr[i] = (byte) (arr[i] ^ (byte) 0x66);
+//                }
+//                outBrowser.write(arr, 0, readbytes);
+//
+//                System.out.printf("done: %d", readbytes);
+//            }
+        }
     }
-    
 }
--- src/javasimpleproxy/Server.java
+++ src/javasimpleproxy/Server.java
@@ -5,10 +5,85 @@
  */
 package javasimpleproxy;
 
+import java.net.*;
+import java.io.*;
+
 /**
  *
  * @author lzp
  */
 public class Server {
-    
+
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        //Socket proxySocket = new Socket("127.0.0.1", 30000);
+
+        ServerSocket ss = new ServerSocket(30001);
+
+        while (true) {
+            Socket clientSocket = ss.accept();
+
+            InputStream clientIn = clientSocket.getInputStream();
+            BufferedOutputStream outClient = new BufferedOutputStream(clientSocket.getOutputStream());
+
+            System.out.println("accept client\n");
+           // Socket proxySocket = new Socket("127.0.0.1", 7201);
+             Socket proxySocket = new Socket("127.0.0.1", 30000);
+            InputStream proxyIn = proxySocket.getInputStream();
+            BufferedOutputStream outProxy = new BufferedOutputStream(proxySocket.getOutputStream());
+
+            System.out.println("connect proxy\n");
+            new Thread(new ThreadedHandler(clientIn, outProxy, clientSocket, proxySocket)).start();
+            new Thread(new ThreadedHandler(proxyIn, outClient, clientSocket, proxySocket)).start();
+        }
+    }
+}
+
+class ThreadedHandler implements Runnable {
+
+    Socket s1;
+    Socket s2;
+    InputStream clientIn;
+    BufferedOutputStream outProxy;
+
+    public ThreadedHandler(InputStream c, BufferedOutputStream o, Socket s1, Socket s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+        clientIn = c;
+        outProxy = o;
+    }
+
+    public void run() {
+        byte[] arr = new byte[8000];
+        int readbytes = 0;
+        try {
+            while ((readbytes = clientIn.read(arr)) > 0) {
+                for (int i = 0; i < arr.length; i++) {
+                    arr[i] = (byte) (arr[i] ^ (byte) 0x66);
+                }
+                outProxy.write(arr, 0, readbytes);
+                outProxy.flush();
+              //  System.out.printf("done: %d\n", readbytes);
+            }
+            
+            s1.close();
+            s2.close();
+            
+            System.out.println("socket close\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+            System.out.println("1\n");
+            try {
+                s1.close();
+                s2.close();
+            } catch (Exception e2) {
+                
+                e2.printStackTrace();
+                
+            System.out.println("2\n");
+            }
+        }
+    }
+
 }
